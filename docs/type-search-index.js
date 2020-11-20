a4e57adc8d41677aec1b8fa0221be7b05c21de42
Rhodrigo <31091322+RhoKratos@users.noreply.github.com>
2020-11-19 20:11:13 -0600

    Add files via upload

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"practica6","l":"Alumno"},{"p":"practica6","l":"Director"},{"p":"practica6","l":"Empleado"},{"p":"practica6","l":"Gerente"},{"p":"practica6","l":"Persona"},{"p":"practica6","l":"Profesor"},{"p":"practica6","l":"Trabajador"}]
